b494e080a7572a2bd2cd28034687a0de15ba8bb5
lmfor <leonardo.e.schaub@gmail.com>
2026-01-18 21:22:28 -0600

    added matches kaggleset

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ import matplotlib.pyplot as plt
 # Files
 from src.data import DatasetConfig, RetrieveData
 
-cfg = DatasetConfig(
+matches_cfg = DatasetConfig(
     dataset="xmorra/lol2020esports",
     file="matches2020.csv",
 )
 
-load = RetrieveData(cfg)
-df = load.load_df()
+champs_cfg = DatasetConfig(
+    dataset="xmorra/lol2020esports",
+    file="champion_stats.csv"
+)
 
-print(df.head())
+matches_loader = RetrieveData(matches_cfg)
+champs_loader = RetrieveData(champs_cfg)
+
+matches_df = matches_loader.load_df()
+champs_df = champs_loader.load_df()
+
+
+print(matches_df.head())
+print(champs_df.head())
