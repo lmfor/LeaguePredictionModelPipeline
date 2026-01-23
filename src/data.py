da3c0361407905da633148bd9b53ddf0c760e837
lmfor <leonardo.e.schaub@gmail.com>
2026-01-22 20:48:00 -0600

    LeagueData class

--- src/data.py
+++ src/data.py
@@ -5,22 +5,31 @@ from typing import Optional, Any, Dict, List
 import pandas as pd
 from pathlib import Path
 
+"""
 @dataclass(frozen=True)
 class DatasetConfig:
-    """
-    Dataset Parameters
-    """
-    
     dataset : str                       # url
     file: Optional[str] = None          # "".csv
     cache_dir : str = "data"            # Storage dir
+"""
     
-    
-class LoadCSV():
+class LeagueData():
     def __init__(self, local_path : str):
         self.csv = pd.read_csv(local_path)
+        # self.team = None
         
-    def _get_team_names(self):
+    def get_csv(self):
+        return self.csv
+        
+    def get_all_team_names(self):
         return self.csv["teamname"].unique()
         
+    def get_teams_by_league(self, league_slug : str):
+        league_games = self.csv[self.csv["league"] == league_slug]
+        league_games = league_games[pd.isna(league_games["playername"])].reset_index(drop=True)
+        return league_games
+    
+    
+    
+    
     
