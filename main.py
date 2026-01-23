464448bdf15e1375cab9694e1f0499fe5b375687
lmfor <leonardo.e.schaub@gmail.com>
2026-01-22 20:47:26 -0600

    data aug experiment

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ import matplotlib.pyplot as plt
 # Files
 # from src.data import DatasetConfig, RetrieveData
 # from train_model.model import Model
-from src.data import LoadCSV
+from src.data import LeagueData
 
-lol2026 = LoadCSV("data\\2026_LoL_esports_match_data_from_OraclesElixir.csv")
+lol2025 = LeagueData("data\\2025_LoL_esports_match_data_from_OraclesElixir.csv")
 
+# all_teams = lol2025.get_all_team_names()
+# lck_teams = lol2025.get_teams_by_league(league_slug='LCK')
 
-print(lol2026._get_team_names())
+lck_games = lol2025.get_teams_by_league('LCK')
+
+print(lol2025.get_csv().columns.tolist())
+# print(lck_games)
+
+        
+    
